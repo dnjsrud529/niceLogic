72a4cc7503373e4721e106a6a4db7b116a56af30
wonkyoung <dnjsrud529@naver.com>
2021-06-05 16:59:07 +0900

    nice second modify

--- nice/src/main/java/com/nice/nice/test/controller/testcontoller.java
+++ nice/src/main/java/com/nice/nice/test/controller/testcontoller.java
@@ -1,12 +1,14 @@
 package com.nice.nice.test.controller;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
@@ -14,11 +16,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import zipUtil.CompressZip;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,38 +33,41 @@ import java.util.List;
 public class testcontoller {
     private String cookie;
     public List<String> data = new ArrayList<>();
+    public List<String> passFiles = new ArrayList<>();
+    public List<String> failFiles = new ArrayList<>();
     private boolean commit = true;
 
-    @RequestMapping(value = "/main.do", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
-        //model.addAttribute("data", "Hello, Spring from IntelliJ! :)");
         data.add("");
-        //model.addAttribute("data",data);
         System.out.println("main in!");
         return "main";
     }
 
-    @RequestMapping(value = "/test.do")
-    public ModelAndView test(){
-        ModelAndView mav = new ModelAndView("main");
-        resultChk("test");
-        mav.addObject("data","taesta");
-        return mav;
-    }
-
-    @RequestMapping(value = "/naver.do")
-    public String naver(){
-        return "redirect:http://naver.com";
+    @RequestMapping(value = "/main.do", method = RequestMethod.GET)
+    public String indexMain(Model model) {
+        data.add("");
+        System.out.println("main in!");
+        return "main";
     }
 
     @RequestMapping(value = "/commit.do", method = RequestMethod.POST)
     public ModelAndView ModelAndView(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView("main");
+        passFiles.clear();
+        failFiles.clear();
+        data.clear();
 
-        String name = request.getParameter("username");
-        String password = request.getParameter("password");
-        String project = request.getParameter("project");
-        String filePath = request.getParameter("filepath");
+        String chk = request.getParameter("chk");
+
+        String name = "admin";
+        String password = "1234qwer!";
+        String project = "zip";
+        String filePath ="";
+        if(chk.equals("a"))
+            filePath = "C:\\Users\\CODEMIND-Romy\\Desktop\\nie\\testsource\\";
+        else
+            filePath = "C:\\Users\\CODEMIND-Romy\\Desktop\\nie\\testsource2\\";
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
         params.add("username",name);
@@ -70,7 +79,7 @@ public class testcontoller {
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/user/login/process", HttpMethod.POST, entity, String.class);
             cookie = responseEntity.getHeaders().get("SET-COOKIE").get(0);
-            data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
+            //data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
 
             response.addHeader("SET-COOKIE", cookie);
             System.out.println(responseEntity.getStatusCode());
@@ -81,13 +90,17 @@ public class testcontoller {
 
             }
             if(commit){
-                data.add("commit ok");
+                data.add("COMMIT OK");
             } else{
-                data.add("commit fail");
+                data.add("COMMIT FAIL");
+                if(failFiles.size() != 0)
+                    data.add(failFiles.size()+"개의 파일에서 10개 이상의 경고가 검출되었습니다.");
             }
             mav.addObject("data", data);
+            mav.addObject("pass",passFiles);
+            mav.addObject("fail",failFiles);
         } catch (Exception e){
-            data.add("fail\n\n");
+            data.add("Fail\n\n");
             mav.addObject("data",data);
         }
         return mav;
@@ -98,43 +111,69 @@ public class testcontoller {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.add("Cookie",cookie);
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
             List<String> files = new ArrayList<>();
 
-            findFile(filepath,files);
+            //findFile(filepath,files);
 
-            String analFiles = "";
-
-            for(int i=0;i<files.size();i++){
-                if(i!=files.size()-1)
-                    analFiles += files.get(i)+",";
-                else
-                    analFiles += files.get(i);
+            CompressZip zip = new CompressZip();
+            try {
+                if (!zip.compress(filepath, filepath, project))
+                { System.out.println("압축 실패"); }
+            } catch (Throwable e) {
+                e.printStackTrace();
             }
+
+            File testfile = new File(filepath+project+".zip");
+//
+           MultipartFile multipartFile = new MockMultipartFile(project, new FileInputStream(testfile));
+//
+
+//
+            ByteArrayResource fileResource = new ByteArrayResource((multipartFile.getBytes())){
+                // 기존 ByteArrayResource의 getFilename 메서드 override
+                @Override
+                public String getFilename() {
+                    return project+".zip";
+                }
+            };
+
+//            String analFiles = "";
+//
+//            for(int i=0;i<files.size();i++){
+//                if(i!=files.size()-1)
+//                    analFiles += files.get(i)+",";
+//                else
+//                    analFiles += files.get(i);
+//            }
 
             MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
             params.add("projectname",project);
-            params.add("filelist",analFiles);
+//            params.add("filelist",analFiles);
+            params.add("file",fileResource);
             HttpEntity entity = new HttpEntity(params,headers);
 
             RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/analyzeCluster", HttpMethod.POST, entity, String.class);
-            data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
+           // ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/analyzeCluster", HttpMethod.POST, entity, String.class);
+            ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/upload", HttpMethod.POST, entity, String.class);
+            //data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
+            Thread.sleep(2000);
             if(analCheck(project).contains("success")) {
-                data.add("anal complete!");
+                //data.add("anal complete!");
                 System.out.println(responseEntity.getStatusCode());
                 System.out.println(responseEntity.getBody());
                 return true;
             }
             else {
-                data.add("anal fail!");
+                //data.add("anal fail!");
                 System.out.println(responseEntity.getStatusCode());
                 System.out.println(responseEntity.getBody());
                 return false;
             }
 
         }catch (Exception e){
-            data.add("anal fail\n\n");
+            //data.add("anal fail\n\n");
             return false;
         }
     }
@@ -146,11 +185,8 @@ public class testcontoller {
 
             HttpEntity entity = new HttpEntity(null,headers);
 
-//            project = "test";
-
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/projects/" + project + "/fileresult", HttpMethod.GET, entity, String.class);
-            //responseEntity.getBody().toString().split("projectName");
             JSONParser parser = new JSONParser();
             JSONArray array = (JSONArray) parser.parse(responseEntity.getBody());
 
@@ -162,40 +198,18 @@ public class testcontoller {
                     if(risky.get(Integer.toString(j))!=null)
                         total += (long)risky.get(Integer.toString(j));
                 }
-                if(total >= 10)
+                if(total >= 10) {
                     commit = false;
-                data.add(obj.get("fileName")+" : "+total);
+                    failFiles.add(obj.get("fileName")+" : "+total+"개의 경고 검출");
+                } else{
+                    passFiles.add(obj.get("fileName")+" : "+total+"개의 경고 검출");
+                }
             }
             System.out.println(responseEntity.getStatusCode()+", "+responseEntity.getBody());
         }catch (Exception e){
-            data.add("get result fail");
+            //data.add("get result fail");
         }
     }
-
-    public class FileResult {
-        String fileName;
-        HashMap<Integer, Integer> risky;
-        String url;
-
-        public FileResult(String f, HashMap<Integer, Integer> r, String u) {
-            fileName = f;
-            risky = r;
-            url = u;
-        }
-
-        public String getFileName() {
-            return fileName;
-        }
-
-        public HashMap<Integer, Integer> getRisky() {
-            return risky;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-    }
-
 
     public String analCheck(String projectname){
         String status = "";
@@ -208,7 +222,7 @@ public class testcontoller {
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/" + projectname + "/status", HttpMethod.GET, entity, String.class);
             status = responseEntity.getBody().split("status\":\"")[1];
-            if (status.contains("progressing") || status.contains("unziping") || status.contains("reserved")) {
+            if (status.contains("progressing") || status.contains("unziping") || status.contains("reserved") || status.contains("onthefly")) {
                 Thread.sleep(5000);
                 status = analCheck(projectname);
             }
@@ -219,15 +233,16 @@ public class testcontoller {
         return status;
     }
 
-    public static void findFile(String path,List<String> filelist){
-        File[] files = new File(path).listFiles();
+//    public static void findFile(String path,List<String> filelist){
+//        File[] files = new File(path).listFiles();
+//
+//        for(File f : files){
+//            if(f.isDirectory()) {
+//                findFile(f.getAbsolutePath(),filelist);
+//            } else {
+//                filelist.add(f.getAbsolutePath());
+//            }
+//        }
+//    }
 
-        for(File f : files){
-            if(f.isDirectory()) {
-                findFile(f.getAbsolutePath(),filelist);
-            } else {
-                filelist.add(f.getAbsolutePath());
-            }
-        }
-    }
 }
--- nice/src/main/webapp/WEB-INF/views/main.jsp
+++ nice/src/main/webapp/WEB-INF/views/main.jsp
@@ -1,13 +1,6 @@
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.ArrayList" %>
-<%--
 
-  Created by IntelliJ IDEA.
-  User: codemind
-  Date: 2021-06-03
-  Time: 오전 11:03
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%--<%--%>
 <%--    List<String> log = new ArrayList<>();--%>
@@ -19,6 +12,7 @@
 <%--        log.add("");--%>
 <%--    }--%>
 <%--%>--%>
+
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -27,30 +21,60 @@
 <body>
     <div>
         <form action="/commit.do" method="post">
-            <input type="text" name="username" placeholder="아이디" value=""/>
-            <input type="password" name="password" placeholder="비밀번호" value=""/>
-            <input type="text" name="project" placeholder="프로젝트이름" value=""/>
-            <input type="text" name="filepath" placeholder="파일경로" value=""/>
-            <input type="submit" value="전송"/>
+                <input type="hidden" name="chk" value="a"/>
+            <input type="submit" value="Commit A"/>
         </form>
     </div>
     <div>
-        <form action="/test.do" method="get">
-
-            <input type="submit" value="전송"/>
+        <form action="/commit.do" method="post">
+            <input type="hidden" name="chk" value="b"/>
+            <input type="submit" id ="btn" value="Commit B"/>
         </form>
+
+
     </div>
-
-
+<div>
 <%
     List<String> log = (List<String>) request.getAttribute("data");
     if(log != null)
     for(String s : log){
+        if(s.contains("commit")){
 %>
-       <p><%=s%>
+    <h2><%=s%></h2>
 <%
+        }else{
+%>
+    <h4><%=s%></h4>
+<%
+            }
     }
 %>
+</div>
+<div>
+    <h3>PassFiles</h3>
+    <%
+        List<String> pass = (List<String>) request.getAttribute("pass");
+        if(pass != null)
+            for(String s : pass){
+    %>
+    <p style="font-size: 15px"><%=s%>
+            <%
+    }
+%>
+</div>
+
+    <div>
+        <h3>FailFiles</h3>
+        <%
+            List<String> fail = (List<String>) request.getAttribute("fail");
+            if(fail != null)
+                for(String s : fail){
+        %>
+        <p style="font-size: 15px"><%=s%>
+                <%
+    }
+%>
+    </div>
 
 </body>
 </html>
